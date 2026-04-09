8dc8738c85c55c9695203f2f5bfd212e105744f6
Samuel Rios <sam.uel20@outlook.com>
2026-04-08 20:37:12 -0400

    Fix Main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,16 +5,15 @@ from app.routes import news_wall
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-   return { "code": 200, "status": "OK" }
-
 app.add_middleware(
    CORSMiddleware,
    allow_origins=["*"],
    allow_credentials=True,
    allow_methods=["*"],
-   allow_headers=["*"]
+   allow_headers=["*"],
 )
-
 app.include_router(news_wall.router)
+
+@app.get("/", methods=["HEAD", "GET"])
+def root():
+   return { "code": 200, "status": "OK" }
